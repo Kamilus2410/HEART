3198a5201b7e6bcfc77e7defc5e7450eb4acf436
Kamilus2410 <Kamilus2410@users.noreply.github.com>
2022-04-22 19:45:40 +0200

    Initial commit

---
+++ src/Heart.java
@@ -0,0 +1,21 @@
+public class Heart {
+
+    public static void main(String[] args) {
+        int n = 47;
+        for (int i = -3*n/2; i <= n; i++) {
+            for (int j = -3*n/2; j <= 3*n/2; j++) {
+
+                // inside either diamond or two circles
+                if ((Math.abs(i) + Math.abs(j) < n)
+                        || ((-n/2-i) * (-n/2-i) + ( n/2-j) * ( n/2-j) <= n*n/2)
+                        || ((-n/2-i) * (-n/2-i) + (-n/2-j) * (-n/2-j) <= n*n/2)) {
+                    System.out.print("♥ ");
+                }
+                else {
+                    System.out.print(". ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
